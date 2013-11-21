cd678c82d4ae93a74182ece5e135be114c292318
Zraz <piotrzientara@Turing.local>
2013-11-21 09:26:25 +0100

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KNKF::Application.config.secret_key_base = '82c309a770cb3a4c836b4a413791bcad7ad7c53309b9ef335bcbdaa59d16ecc38d9e45382715bc90c232fee8ecb2bc69c96b36ad6d670d5f750112fdb5183081'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KNKF::Application.config.session_store :cookie_store, key: '_KNKF_session'
