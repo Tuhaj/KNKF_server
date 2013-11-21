5c3080b20b59c79815b9932b4ed7a783dd62dbcc
Zraz <piotrzientara@Turing.local>
2013-11-21 17:08:39 +0100

    tests working

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe "Model User" do
 
 	before(:all) do
-		@user = User.new
+		@user = User.new email:"asd@asd.pl", password:"asdasdasd"
 		@meeting = Meeting.new
 		@lecture = Lecture.new
 		@application = Application.new
@@ -13,7 +13,9 @@ describe "Model User" do
 		user = User.new
 		user.email = "zrazic@wp.pl"
 		user.full_name = "Piotrek"
-		expect { @user.save }.to change { User.count }.by 1
+		user.password = "asdasdasd"
+
+		expect { @user.save! }.to change { User.count }.by 1
 	end
 
 	it "has association with meeting" do
