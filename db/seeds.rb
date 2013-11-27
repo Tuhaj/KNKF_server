645bace1f5504c71d70d5e0e0300215da6bc5cb0
Zraz <zrazic@wp.pl>
2013-11-27 12:01:19 +0100

    devise dictionary updated to whole Polish

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,9 +7,9 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-# Meeting.create(name:"bedzie2", date:"2014-01-13")
-# Meeting.create(name:"bedzie2", date:"2013-12-13")
-# Meeting.create(name:"bylo", date:"2012-01-13")
+ Meeting.create(name:"bedzie2", date:"2014-01-13")
+ Meeting.create(name:"bedzie2", date:"2013-12-13")
+ Meeting.create(name:"bylo", date:"2012-01-13")
 #User.create(full_name: "Piotr", degree: "MA", password: "mama1234", email: "piotr.zientara1@gmail.com")
 #User.create(full_name: "Hieronim", degree: "Professor", password: "mama1234", email: "zrazic@wp.pl")
 Admin.create(email: "zrazic@wp.pl", password: "Ubuntu618")
