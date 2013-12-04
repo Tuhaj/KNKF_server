71394006eebcf2f26f282e754bcb18f7ba95cbbe
Zraz <zrazic@wp.pl>
2013-12-04 12:03:36 +0100

    testing solution to mailer failure

--- config/environment.rb
+++ config/environment.rb
@@ -3,4 +3,5 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 KNKF::Application.initialize!
-config.action_mailer.default_url_options = { :host => 'localhost' }
+#config.action_mailer.default_url_options = { :host => 'localhost' }
+config.action_mailer.default_url_options = { :host => 'koloksztalceniafilozoficznego.herokuapp.com' }
