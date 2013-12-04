2ab18758b9ccad4d5e8175838749c5e310310155
Zraz <zrazic@wp.pl>
2013-12-04 11:55:57 +0100

    found error in password recovery

--- spec/models/admin_spec.rb
+++ spec/models/admin_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe Admin do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+		before(:all) do
+		@admin = Admin.new email:"zrazic@wp.pl", password:"asdasdasd"
+	end
+
+	it "Admin can be created" do
+  	admin = @admin
+  	@admin.email.should = zrazic@wp.pl
+
+	end
+
 end
