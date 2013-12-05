2b338cc584a0deba4c6ddaa7b6fffa2371e58239
Zraz <zrazic@wp.pl>
2013-12-06 00:13:32 +0100

    password to mail hidden and changed

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -11,8 +11,8 @@ KNKF::Application.configure do
      port: 587,
      domain: "gmail.com",
      authentication: "plain",
-     user_name: "knkf.uw@gmail.com",
-     password: "Nietzsche"
+     user_name: ENV["GMAIL_USERNAME"],
+     password: ENV["GMAIL_PASSWORD"]
 }
 
   # In the development environment your application's code is reloaded on
