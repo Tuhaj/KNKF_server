2ea844b6e2feca49e7205d84f014f13391926057
Zraz <zrazic@wp.pl>
2013-12-18 10:38:01 +0100

    drying with helpers - reading and meeting functions

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -6,4 +6,24 @@ module UserHelper
       ""
     end
   end  
+
+  def membership_info(user)
+    if user.knkf_member?
+      "KNKF"
+    else
+      "gość"
+    end
+  end
+
+  def sign_out_from_meeting(user)
+    if user == current_user
+      link_to 'wypisz się', meeting_remove_me_path(@meeting), class: "btn btn-link", :method =>:delete
+    else
+      ""
+    end
+  end
+
+  def sign_in_or_out_to_meeting(user)
+  end
+
 end
