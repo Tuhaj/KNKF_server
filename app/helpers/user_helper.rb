120b736cf2e413e7ed7103333f3e87249b113b66
Zraz <zrazic@wp.pl>
2013-12-18 16:24:49 +0100

    translation in progress, views: meeting

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,15 +1,15 @@
 module UserHelper
   def user_name(user)
-    user.present? ? "Zaproponowane przez: " + user.full_name : ""
+    user.present? ? t("meeting.recommended") + user.full_name : ""
   end  
 
   def membership_info(user)
-    user.knkf_member? ? "KNKF" : "gość"
+    user.knkf_member? ? "KNKF" : t('guest')
   end
 
   def sign_out_from_meeting(user)
     if user == current_user
-      link_to 'wypisz się', meeting_remove_me_path(@meeting), class: "btn btn-link", :method =>:delete
+      link_to t('meeting.sign_out'), meeting_remove_me_path(@meeting), class: "btn btn-link", :method =>:delete
     else
       ""
     end
@@ -17,15 +17,15 @@ module UserHelper
 
   def relation_to_meeting(meeting)
     if meeting.is_user_in_meeting?(current_user)
-      link_to 'wypisz się', meeting_remove_me_path(meeting), class: "btn btn-link", method: :delete
+      link_to t('meeting.sign_out'), meeting_remove_me_path(meeting), class: "btn btn-link", method: :delete
     else
-      link_to 'weź udział', meeting_add_me_path(meeting), class: "btn btn-link", method: :post
+      link_to t('meeting.take_part'), meeting_add_me_path(meeting), class: "btn btn-link", method: :post
     end
   end
 
   def destroy_meeting_if_can(meeting)
     if can? :destroy, meeting
-      link_to('Usuń', meeting, class: "btn btn-link", method: :delete, data: { confirm: t("confirm_deletion") }) + link_to('Edytuj', edit_meeting_path(meeting), class: "btn btn-link")
+      link_to(t('delete'), meeting, class: "btn btn-link", method: :delete, data: { confirm: t("meeting.confirm_deletion") }) + link_to(t('edit'), edit_meeting_path(meeting), class: "btn btn-link")
     else
       ""
     end
@@ -33,7 +33,7 @@ module UserHelper
 
   def display_destroy_reading(reading)
     if can? :destroy, reading
-      link_to('Skasuj', reading, method: :delete, data: { confirm: 'Czy na pewno chcesz usunąć?' })
+      link_to(t('delete'), reading, method: :delete, data: { confirm: t("meeting.confirm_deletion") })
     else
       ""
     end
@@ -41,9 +41,9 @@ module UserHelper
 
   def heading_my_or_all
     if @only_mine
-      "Twoje spotkania"
+      t("meeting.your_meetings")
     else
-      "Nadchodzące spotkania"
+      t("meeting.incomming_meetings")
     end
   end
 end
