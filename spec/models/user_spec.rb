c7fd5b10f4e276d4d0907169f258fd633a551e4e
Zraz <zrazic@wp.pl>
2013-12-30 10:44:33 +0100

    models spec dried and corrected

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,35 +1,32 @@
 require 'spec_helper' 
 
 describe "Model User" do
-
-	before(:all) do
-		@user = User.new email:"asd@asd.pl", password:"asdasdasd"
-		@meeting = Meeting.new
-		@lecture = Lecture.new
-		@message = Message.new
-	end
+let!(:user) {create(:user)}
 
 	it "User can be created" do
-		user = User.new
-		user.email = "zrazic@wp.pl"
-		user.full_name = "Piotrek"
-		user.password = "asdasdasd"
+		@user = User.new
+		@user.email = "tester@wp.pl"
+		@user.full_name = "Piotrek"
+		@user.password = "asdasdasd1"
 
 		expect { @user.save! }.to change { User.count }.by 1
 	end
 
 	it "has association with meeting" do
+		@meeting = Meeting.new
 		@meeting.users.build
 		@meeting.users.should_not be_nil
 	end
 	it "has association with lecture" do
+		@lecture = Lecture.new
 		@lecture.users.build
 		@lecture.users.should_not be_nil
 	end
 
 	it "has association with application" do
-		@user.messages.build
-		@user.messages.should_not be_nil
+		@message = Message.new
+		user.messages.build
+		user.messages.should_not be_nil
 	end
 
 
