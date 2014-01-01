5dfda5c0d3309ff155a890864b467c77c0645a02
Zraz <zrazic@wp.pl>
2014-01-01 13:40:39 +0100

    tests clean up

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -4,6 +4,7 @@ describe Message do
   let(:message) {create(:message)}
   let(:user) {create(:user)}
   subject { message }
+  
   its(:title) {should eql "tytuł wiadomości"}
   its(:membership) {should be_false}
   its(:content) {should eql "Wiadomość"}
