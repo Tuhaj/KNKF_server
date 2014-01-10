29a7a599323e86055e4059ad7da01e368714e513
Zraz <zrazic@wp.pl>
2014-01-10 13:16:41 +0100

    evernote auth working, missing permanent token for app

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -15,7 +15,7 @@ class NotesController < ApplicationController
 
  #     auth_token = "S=s1:U=8d910:E=14aa5c00ee7:C=1434e0ee2e9:P=1cd:A=en-devtoken:V=2:H=f1e3575f9b4a41562505846d4e8756be"
       client = EvernoteOAuth::Client.new
-      request_token = client.request_token(:oauth_callback => oauth_callback_reading_note_url(@reading, @note))
+      session[:request_token] = request_token = client.request_token(:oauth_callback => oauth_callback_reading_note_url(@reading, @note))
       url = request_token.authorize_url
       
       
@@ -40,20 +40,21 @@ class NotesController < ApplicationController
 
   def oauth_callback
     @note = Note.find(params[:id]) 
-    session[:token] = token = params[:oauth_token]
-    client = EvernoteOAuth::Client.new(token: token)
-    begin
+    @reading = Reading.find(params[:reading_id])
+    access_token = session[:request_token].get_access_token(:oauth_verifier => params[:oauth_verifier])
+    
+    client = EvernoteOAuth::Client.new(token: access_token.token)
+
       note_store = client.note_store
       notebooks = note_store.listNotebooks
+      
       note = Evernote::EDAM::Type::Note.new
       note.title = "#{@note.title}"
       part1 = '<?xml version="1.0" encoding="UTF-8"?><!DOCTYPE en-note SYSTEM "http://xml.evernote.com/pub/enml2.dtd"><en-note>'
       part2 = '<br/></en-note>'
       note.content =  part1 + @note.content + part2
       note_store.createNote(note)
-    rescue Evernote::EDAM::Error::EDAMUserException => ex
-      raise ex.inspect
-    end
+
 
     redirect_to @reading, :notice => "Udało się dodać notatkę"
   end
