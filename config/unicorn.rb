292023e6071b6a1b74de076c156e28f8e08cd7c7
Zraz <zrazic@wp.pl>
2014-01-23 11:11:34 +0100

    unicorn - set up for cooperation with nginx in progress

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,7 +6,7 @@
 # See also http://unicorn.bogomips.org/examples/unicorn.conf.rb for
 # a more verbose configuration using more features.
 
-listen 3000 # by default Unicorn listens on port 8080
+# listen 3000 # by default Unicorn listens on port 8080 I comment it out so gets request from NginX
 worker_processes 2 # this should be >= nr_cpus
 pid "/home/vagrant/app/shared/tmp/pids/unicorn.pid"
 stderr_path "./log/unicorn.log"
