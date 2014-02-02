9fc6eecd9cfcda8aaffbee228052940429f9f67e
Zraz <zrazic@wp.pl>
2014-02-02 15:48:22 +0100

    translations corrected, code refactoring in progress

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def display_login_button
+   current_user.knkf_member ? t('log_in_as_knkf') : t('log_in_as_guest')
+  end
+
+  def permitions_title
+    current_user.knkf_member ? t('knkf_permissions') : t('guest_permissions')
+  end
+
 end
